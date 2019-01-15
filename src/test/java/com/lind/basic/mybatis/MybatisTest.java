cc4d317aa7e5d2d26705ceb5b7b9ff23fc5b5006
zhangzl <zhangzl@i-counting.cn>
2019-01-15 17:53:03 +0800

    mybatis selectList,selectPage

--- src/test/java/com/lind/basic/mybatis/MybatisTest.java
+++ src/test/java/com/lind/basic/mybatis/MybatisTest.java
@@ -1,6 +1,9 @@
 package com.lind.basic.mybatis;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.lind.basic.BaseTest;
+import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -15,5 +18,23 @@ public class MybatisTest extends BaseTest {
         .email("test@sina.com")
         .build();
     userInfoMapper.insert(userInfo);
+    System.out.println("userinfo:" + userInfo.toString());
+  }
+
+  @Test
+  public void findList() {
+    QueryWrapper<UserInfo> queryWrapper = new QueryWrapper<>();
+    queryWrapper.eq("name", "lind");
+    Assert.assertEquals(0, userInfoMapper.selectList(queryWrapper).size());
+  }
+
+  @Test
+  public void findPage() {
+    QueryWrapper<UserInfo> queryWrapper = new QueryWrapper<>();
+    queryWrapper.eq("name", "lind");
+    Assert.assertEquals(0, userInfoMapper.selectPage(
+        new Page<>(1, 10),
+        queryWrapper)
+        .getSize());
   }
 }
--- src/test/java/com/lind/basic/mybatis/UserInfo.java
+++ src/test/java/com/lind/basic/mybatis/UserInfo.java
@@ -2,9 +2,11 @@ package com.lind.basic.mybatis;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
 @Builder(toBuilder = true)
+@ToString
 public class UserInfo {
   private Long id;
   private String name;
