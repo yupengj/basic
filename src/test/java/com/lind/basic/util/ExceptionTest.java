fb81ceba93821635fa1adb75260f3fdf8938f1ec
zhangzl <zhangzl@i-counting.cn>
2019-04-09 11:11:15 +0800

    添加ex相关

--- src/test/java/com/lind/basic/util/ExceptionTest.java
+++ src/test/java/com/lind/basic/util/ExceptionTest.java
@@ -14,4 +14,14 @@ public class ExceptionTest {
       System.out.println("success end.");
     }
   }
+
+  @Test()
+  public void throwTest() {
+    try {
+      int a = 0;
+      int b = 1 / a;
+    } catch (Exception ex) {
+      ex.printStackTrace();//只看控制台输入堆栈信息并没有throw
+    }
+  }
 }
